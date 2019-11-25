5ba790768f21f87b3f6c5c7719f66216fabc571b
ronyantony00 <ronyantony00@gmail.com>
2019-11-25 18:09:37 +0530

    4 files modified

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JavaScript Algorithms And Data Structures</h1>`;
+appDiv.innerHTML = `JavaScript Algorithms And Data Structures`;
