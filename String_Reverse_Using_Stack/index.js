449da9cd4d3401a6fb4d229c875e991c7a7f2eb8
ronyantony00 <ronyantony00@gmail.com>
2019-11-26 05:45:46 +0530

    2 files modified, 1 added, 2 deleted

---
+++ String_Reverse_Using_Stack/index.js
@@ -0,0 +1,42 @@
+
+class Stack{
+  
+  constructor(){
+    this.items = [];
+  }
+   
+  addToStack(element){
+    this.items.push(element);
+  }
+  
+  popFromStack(){
+    if(this.items.length==0){
+      return "Underflow";
+    }
+    return this.items.pop();
+  }
+  
+  isEmpty(){
+    return this.items.length==0;
+  }
+  
+}
+
+reverseString = (reversedWord) => {
+  let stack = new Stack();
+  let reversedString = "";
+  for(let i=0;i<reversedWord.length; i++){
+    stack.addToStack(reversedWord[i]);
+  };
+  while(!stack.isEmpty()){
+    reversedString = reversedString + stack.popFromStack();
+  }
+  return reversedString;
+  // results = results + "<h3>"+reversedString+"</h3>";
+  // resultDiv.innerHTML = results;
+}
+
+export { reverseString };
+
+// results = "String To be reversed: <h4>ABCDEFGHIJK</h4>";
+// reverseString("ABCDEFGHIJK");
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Import stylesheets
 import './style.css';
+import { reverseString } from './String_Reverse_Using_Stack/index.js';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `JavaScript Algorithms And Data Structures`;
+
+window.stringReverseUsingStack = () => {
+  alert(reverseString("ABCDEFGHIJK"));
+}
